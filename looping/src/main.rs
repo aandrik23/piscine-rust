a7f3e75ff5fa4cc95892dc75c2a01bae032a3906
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-05-05 20:48:29 +0300

    looping_try2

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 fn main() {
-    let riddle = "I am the begging of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
     let answer = "the letter e";
 
     let mut attempts = 0;
@@ -14,8 +14,13 @@ fn main() {
         let user_input = input.trim().to_lowercase();
 
         if user_input == answer {
-            println!("Correct! You solved the riddle in {} attempts.", attempts);
+
+            println!("Number of trials: {}", attempts);
+
             break;
+
         }
+
     }
+
 }
