0df5d3a8017da497dcca61b9528a058ad22bcc3c
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-05-06 20:04:43 +0300

    doubtful

---
+++ doubtful/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn doubtful(s: &mut String) {
+    s.push('?');
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_doubtful() {
+        let mut s = "Hello".to_owned();
+
+        doubtful(&mut s);
+
+        assert_eq!(s, "Hello?");
+    }
+}
