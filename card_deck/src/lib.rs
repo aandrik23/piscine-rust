e13e718133be2c0636b625a980520159260d679f
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-05-07 01:28:59 +0300

    card deck2

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,28 +1,16 @@
 use rand::Rng;
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Suit {
-    Heart,
-    Diamond,
-    Spade,
-    Club,
-}
+#[derive(Debug, PartialEq)]
+pub enum Suit { Heart, Diamond, Spade, Club }
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Rank {
-    Ace,
-    King,
-    Queen,
-    Jack,
-    Number(u8),
-}
+#[derive(Debug, PartialEq)]
+pub enum Rank { Ace, King, Queen, Jack, Number(u8) }
 
 impl Suit {
-    pub fn random() -> Suit {
-        let value = rand::thread_rng().gen_range(1..=4);
-
-        Suit::translate(value)
-    }
+    pub fn random() -> Suit { 
+                let mut rng = rand::thread_rng();
+        Suit::translate(rng.gen_range(1u8..=4u8))
+     }
 
     pub fn translate(value: u8) -> Suit {
         match value {
@@ -35,50 +23,28 @@ impl Suit {
 }
 
 impl Rank {
-    pub fn random() -> Rank {
-        let value = rand::thread_rng().gen_range(1..=13);
-
-        Rank::translate(value)
+    pub fn random() -> Rank {        
+         let mut rng = rand::thread_rng();
+        Rank::translate(rng.gen_range(1u8..=13u8))
     }
 
     pub fn translate(value: u8) -> Rank {
         match value {
-            1 => Rank::Ace,
-            11 => Rank::Jack,
-            12 => Rank::Queen,
-            13 => Rank::King,
-            n => Rank::Number(n),
+            1      => Rank::Ace,
+            11     => Rank::Jack,
+            12     => Rank::Queen,
+            13     => Rank::King,
+            n      => Rank::Number(n),
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
 }
 
 pub fn winner_card(card: &Card) -> bool {
-    matches!(card.suit, Suit::Spade)
-        && matches!(card.rank, Rank::Ace)
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn main() {
-    let your_card = Card {
-        rank: Rank::random(),
-        suit: Suit::random(),
-    };
-
-    println!("Your card is {:?}", &your_card);
-
-    if card_deck::winner_card(&your_card) {
-        println!("You are the winner!");
-    }
-}
+    card.suit == Suit::Spade && card.rank == Rank::Ace
 }
