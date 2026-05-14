c552671b277815c82053da884dd7f14fc29a263f
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-05-14 20:40:21 +0300

    talking

---
+++ talking/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn talking(text: &str) -> &str {
+    if text.is_empty() {
+        return "Just say somthing!";
+    }
+
+    let is_question = text.ends_with('?');
+    let has_letters = text.chars().any(|c| c.is_alphabetic());
+    let is_yelling = has_letters && text.chars().filter(|c| c.is_alphabetic()).all(|c| c.is_uppercase());
+
+    
+    if is_yelling && is_question {
+        "Quiet, I am thinking!"
+    } else if is_yelling {
+        "There is no need to yell, calm down!"
+    } else if is_question {
+        "Sure."
+    } else {
+        "Interesting"
+    }
+}
