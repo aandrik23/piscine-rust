7308181d600476da0d2540dfeedc405b77bbd2d4
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-05-19 18:44:11 +0300

    borrow box

---
+++ borrow_box/src/lib.rs
@@ -0,0 +1,46 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct GameSession {
+    pub id: u32,
+    pub p1: (String, u32),
+    pub p2: (String, u32),
+    pub nb_games: u32,
+}
+
+impl GameSession {
+    pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u32) -> GameSession {
+        GameSession {
+            id,
+            p1: (p1_name, 0),
+            p2: (p2_name, 0),
+            nb_games,
+        }
+    }
+
+    pub fn read_winner(&self) -> Option<&(String, u32)> {
+        let needed_score = self.nb_games / 2 + 1;
+
+        if self.p1.1 >= needed_score {
+            Some(&self.p1)
+        } else if self.p2.1 >= needed_score {
+            Some(&self.p2)
+        } else {
+            None
+        }
+    }
+
+    pub fn update_score(&mut self, user_name: &str) {
+        if self.read_winner().is_some() {
+            return;
+        }
+
+        if self.p1.0 == user_name {
+            self.p1.1 += 1;
+        } else if self.p2.0 == user_name {
+            self.p2.1 += 1;
+        }
+    }
+
+    pub fn delete(self) -> String {
+        format!("game deleted: id -> {}", self.id)
+    }
+}
