6dea2176481711c16579a665bb3274f1cedac8e9
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-05-19 18:55:27 +0300

    borrow box3

--- borrow_box/src/lib.rs
+++ borrow_box/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct GameSession {
     pub id: u32,
@@ -7,42 +9,51 @@ pub struct GameSession {
 }
 
 impl GameSession {
+    #[inline]
     pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u32) -> GameSession {
         GameSession {
             id,
-            p1: (p1_name, 0),
-            p2: (p2_name, 0),
+            p1: (p1_name, Default::default()),
+            p2: (p2_name, Default::default()),
             nb_games,
         }
     }
 
+    #[inline]
     pub fn read_winner(&self) -> Option<&(String, u32)> {
-        let max_score = self.nb_games / 2 + 1;
-
-        if self.p1.1 == max_score {
-            Some(&self.p1)
-        } else if self.p2.1 == max_score {
-            Some(&self.p2)
-        } else {
-            None
+        match self.p1.1.cmp(&self.p2.1) {
+            Ordering::Greater => Some(&self.p1),
+            Ordering::Less => Some(&self.p2),
+            Ordering::Equal => None,
         }
+    }
+
+    #[inline]
+    fn game_ended(&self) -> bool {
+        self.p1.1 * 2 > self.nb_games
+            || self.p2.1 * 2 > self.nb_games
+            || self.p1.1 + self.p2.1 == self.nb_games
     }
 
     pub fn update_score(&mut self, user_name: &str) {
-        if self.read_winner().is_some() {
+        if self.game_ended() {
             return;
         }
 
-        if self.p1.0 == user_name {
-            self.p1.1 += 1;
-        }
+        let player = if self.p1.0 == user_name {
+            &mut self.p1
+        } else if self.p2.0 == user_name {
+            &mut self.p2
+        } else {
+            return;
+        };
 
-        if self.p2.0 == user_name {
-            self.p2.1 += 1;
-        }
+        player.1 += 1;
     }
-    
+
     pub fn delete(self) -> String {
-        format!("game deleted: id -> {}", self.id)
+        let id = self.id;
+
+        format!("game deleted: id -> {id}")
     }
 }
