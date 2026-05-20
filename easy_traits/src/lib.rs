d6a8ecefb86a63a9ce2e09447f231d6f3f575a46
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-05-20 19:25:41 +0300

    easy traits

---
+++ easy_traits/src/lib.rs
@@ -0,0 +1,24 @@
+pub trait AppendStrExt {
+    fn append_str(&mut self, str_to_append: &str) -> &mut Self;
+
+    fn append_number(&mut self, nb_to_append: f64) -> &mut Self;
+
+    fn remove_punctuation_marks(&mut self) -> &mut Self;
+}
+
+impl AppendStrExt for String {
+    fn append_str(&mut self, str_to_append: &str) -> &mut Self {
+        self.push_str(str_to_append);
+        self
+    }
+
+    fn append_number(&mut self, nb_to_append: f64) -> &mut Self {
+        self.push_str(nb_to_append.to_string().as_str());
+        self
+    }
+
+    fn remove_punctuation_marks(&mut self) -> &mut Self {
+        self.retain(|c| !matches!(c, '.' | ',' | '!' | '?'));
+        self
+    }
+}
