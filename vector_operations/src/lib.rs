c27b2d88ecdadd922059d0e2f7aab0cf2c8afc23
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-05-21 19:19:27 +0300

    vector operations

---
+++ vector_operations/src/lib.rs
@@ -0,0 +1,32 @@
+use std::ops::{Add, Sub};
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct ThreeDVector<T> {
+    pub i: T,
+    pub j: T,
+    pub k: T,
+}
+
+impl<T: Add<Output = T>> Add for ThreeDVector<T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            i: self.i + rhs.i,
+            j: self.j + rhs.j,
+            k: self.k + rhs.k,
+        }
+    }
+}
+
+impl<T: Sub<Output = T>> Sub for ThreeDVector<T> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            i: self.i - rhs.i,
+            j: self.j - rhs.j,
+            k: self.k - rhs.k,
+        }
+    }
+}
